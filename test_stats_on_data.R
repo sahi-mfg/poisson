3705678c41b3c3aa713f36642cb21b429e7db2b7
Sahi Mohamed Francis Gonsangbeu <mohamedfrancissahi@gmail.com>
2023-12-15 11:13:10 +0100

    Refactor file paths and variable names, and calculate lambda_hat

--- test_stats_on_data.R
+++ test_stats_on_data.R
@@ -1,15 +1,14 @@
-data1 <- read.table("data/1.txt")
-data2 <- read.table("data/2.txt")
-data3 <- read.table("data/3.txt")
+data1 <- read.table("data/data1.txt")
+data2 <- read.table("data/data2.txt")
+data3 <- read.table("data/data3.txt")
 
 data <- rbind(data1, data2, data3)
-
-data <- data[order(data$V1), ]
-
+data <- as.data.frame(data)
+colnames(data) <- "V1"
 # On calcule les temps inter-arrivées
 temps_inter_arrivee <- diff(data$V1)
 
-lamba_hat <- 1 / mean(temps_inter_arrivee)
+lambda_hat <- 1 / mean(temps_inter_arrivee)
 
 # test de Kolmogorov-Smirnov
 ks.test(temps_inter_arrivee, "pexp", lambda_hat)
